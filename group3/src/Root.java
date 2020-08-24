1bef38f50867a1d99c569294ea3f14476be5187e
jinqiuyi <877725256@qq.com>
2020-08-24 16:35:29 +0800

    Add files via upload

---
+++ group3/src/Root.java
@@ -0,0 +1,9 @@
+
+public class Root {
+
+	public static void main(String[] args) {
+		System.out.println("root3");
+
+	}
+
+}
---
+++ group3/src/hello.java
@@ -0,0 +1,12 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+<<<<<<< HEAD
+		System.out.println("test 也修改");
+=======
+		System.out.println("root修改同一个文件");
+>>>>>>> 我先提交
+	}
+
+}
